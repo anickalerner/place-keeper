d32004f3e7c46dee7cfa4e55ad7d8e99684e112a
Anna Lerner <anicka.lerner@gmail.com>
2020-08-06 07:47:27 +0300

    Adding Google Map

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,5 +1,14 @@
 'use strict';
+let map;
 
+    function initMap() {
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 29.6274085, lng: 34.883855 },
+        zoom: 8
+      });
+    }
 $(document).ready(function(){
+    
+
 
 });
